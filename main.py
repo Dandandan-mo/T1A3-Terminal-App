276f549bdba6183c1405f741f96a612efa25fadb
Dandandan-mo <dan.mo6304@gmail.com>
2022-09-22 06:02:49 +1000

    amend wording

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import features as f
 
-print('''
-Welcome to the budget tracker!
-This app helps you track your incomes and expenses.
-''')
+print('Welcome to the budget tracker!\n')
 
 f.add(f.Transactions.deposit, f.income_category, 'income')
 f.add(f.Transactions.withdraw, f.expense_category, 'expense')

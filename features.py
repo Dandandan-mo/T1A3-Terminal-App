0e79fd7ca8cd2f90076356d609a7cd4c6c3689ec
Dandandan-mo <dan.mo6304@gmail.com>
2022-09-22 16:47:25 +1000

    modified functions and methods

--- features.py
+++ features.py
@@ -1,6 +1,7 @@
 from datetime import date
 import cowsay
-import multicurrency
+from multicurrency import AustralianDollar, USDollar, Euro, Yuan
+import colorama
 
 details = []
 
@@ -11,30 +12,13 @@ class Category:
     def __init__(self, category_list):
         self.category_list = category_list
 
-    def instruction(self, section):
-        # return int(input
-        return int(input(f'\nEnter an integer number to add an {section} in one of the above categories.\nEnter any integer other than 0 or the ones listed to customise a new category. \nEnter 0 when finish adding all {section} transactions. '))
-
-    def options(self, category_list):
-        for count, items in enumerate(category_list):
+    def options(self):
+        for count, items in enumerate(self.category_list):
             print(count+1, items)
 
-    def new_category(self, category_list):
+    def new_category(self):
         new = input('Creating a new category. Please enter the name of the new category: ')
-        return category_list.append(new.capitalize())
-
-    def show_balance(self):
-        title = "-"*23 + "Budget Report" + "-"*23 + "\n"
-        items = ""
-        total = 0
-        date_printed = date.today()
-        for item in details:
-            items += f"{item['category'][0:20]:20}" + f"{item['description']:30}" + f"{item['amount']:>9.2f}" + "\n"
-
-            total += item['amount']
-
-        output = title + items + "\nBalance: " + str(total) + "\nDate Printed: " + str(date_printed)
-        print(output)
+        return self.category_list.append(new.capitalize())
 
 class Transactions:
     def __init__(self, category, description, amount):
@@ -49,14 +33,18 @@ class Transactions:
     def withdraw(self):
         self.details.append({'category': self.category, 'description': self.description, 'amount': -self.amount})
 
+def instruction(section):
+        return int(input(f'\nEnter an integer number to add an {section} in one of the above categories.\nEnter any integer other than 0 or the ones listed to customise a new category. \nEnter 0 when finish adding all {section} transactions. '))
+
 def add(deposit_or_withdraw, income_or_expense, section):
     while True:
         print('-'*59)
+        print(f'{section.upper()}\n')
         display = Category(income_or_expense)
-        display.options(income_or_expense)
+        display.options()
 
         try:
-            x = display.instruction(section)
+            x = instruction(section)
     
             if x in range (1, len(income_or_expense)+1):
                 category = income_or_expense[x-1]
@@ -75,7 +63,26 @@ def add(deposit_or_withdraw, income_or_expense, section):
                 break
     
             elif x < 0 or x >= len(income_or_expense)+1:
-                display.new_category(income_or_expense)
+                display.new_category()
     
         except ValueError:
             cowsay.cow('Please enter an integer.')
+
+def balance(category):
+        total = 0
+        for item in details:
+            if category == item['category']:
+                total += item['amount']
+        print(f'{category} subtotal: {total}')
+
+def show_balance():
+        title = "-"*23 + "Budget Report" + "-"*23 + "\n"
+        items = ""
+        total = 0
+        date_printed = date.today()
+        for item in details:
+            items += f"{item['category'][0:20]:20}" + f"{item['description']:30}" + f"{item['amount']:>9.2f}" + "\n"
+            total += item['amount']
+
+        output = title + items + "\nBalance: " + str(total) + "\nDate Printed: " + str(date_printed) + "\n"
+        print(output)
--- main.py
+++ main.py
@@ -5,6 +5,10 @@ f.cowsay.cow('Welcome to the budget calculater!\n')
 f.add(f.Transactions.deposit, f.income_category, 'income')
 f.add(f.Transactions.withdraw, f.expense_category, 'expense')
 
-f.Category(f.income_category.append(f.expense_category)).show_balance()
+f.cowsay.cow('Calculation done. This is your budget report')
+
+f.show_balance()
+
+f.balance('Food')
 
 
